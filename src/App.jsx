e60ed22d9b23d65e68c0f6d41e5a4fb998377c90
Stuarte.exe <157625546+EmilioStuart@users.noreply.github.com>
2025-10-01 10:41:57 -0300

    Subindo o FinalChallenge

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TemplateExpressions from './components/TemplateExpressions'
 import MyComponent from './components/MyComponent'
 import Events from './components/Events'
 import Challenge from './components/Challenge'
+import FinalChallenge from './components/FinalChallenge'
 
 function App() {
   return (
@@ -12,10 +13,13 @@ function App() {
       <h1>Fundamentos React</h1>
       <FirstComponent />
       <TemplateExpressions />
-      <MyComponent/>
-      <MyComponent/>
-      <Events/>
-      <Challenge/>
+      <MyComponent />
+      <MyComponent />
+      <Events />
+      <Challenge />
+      <FinalChallenge x={5} y={10} />
+      <FinalChallenge x={25} y={30} />
+      <FinalChallenge x={85} y={1} />
     </div>
   )
 }
---
+++ src/components/FinalChallenge.jsx
@@ -0,0 +1,10 @@
+// src/components/FinalChallenge.jsx
+
+const FinalChallenge = ({ x, y }) => {
+  return (
+    <div>
+      <h3>Desafio Final</h3>
+      <p>A soma de {x} e {y} é {x + y}</p>
+    </div>
+  )
+}
